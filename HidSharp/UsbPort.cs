d75d318b82226752d74c16acfa7f224f7665db4d
James <>
2025-10-08 10:34:58 +0000

    Adding version 2.6.3 to the new source control system.
    FossilOrigin-Name: 69b77809b5c0349962603e685527ccb858942d7493d17c1d24ba43b32953eb54

---
+++ HidSharp/UsbPort.cs
@@ -0,0 +1,20 @@
+﻿namespace HidSharp
+{
+    public abstract class UsbPort
+    {
+        public override string ToString()
+        {
+            return string.Format("{0} port {1}", HubDevicePath, PortNumber);
+        }
+
+        public abstract string HubDevicePath
+        {
+            get;
+        }
+
+        public abstract int PortNumber
+        {
+            get;
+        }
+    }
+}
